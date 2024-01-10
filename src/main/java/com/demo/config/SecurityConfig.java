9be2434e77cd81bed1cc3fc3aae48c6fba8fd814
dongbum.ku <db1223@naver.com>
2024-01-11 05:31:58 +0900

    SAMLResponse 응답 구현

--- src/main/java/com/demo/config/SecurityConfig.java
+++ src/main/java/com/demo/config/SecurityConfig.java
@@ -1,62 +1,66 @@
-package com.demo.config;
-
-import java.io.IOException;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> 
-				// request List
-				authorize.requestMatchers("admin").permitAll()
-					.requestMatchers("/public/**").permitAll()
-					.anyRequest().authenticated())
-				// formLogin
-				.formLogin(formLogin -> formLogin
-						.loginPage("/loginPage") // Login URL
-						.defaultSuccessUrl("/main") // 인증 후 URL -> default : 이전 URL
-						.failureUrl("/error/401")
-						.usernameParameter("userId") // ID parameter name
-						.passwordParameter("passwd") // PWD parameter name
-						.loginProcessingUrl("/login_proc") // Login Form Action URL
-						.successHandler(new AuthenticationSuccessHandler() {
-							@Override
-							public void onAuthenticationSuccess(HttpServletRequest request,
-									HttpServletResponse response, Authentication authentication)
-									throws IOException, ServletException {
-								System.out.println("authentication : " + authentication.getName());
-								response.sendRedirect("/main");
-							}
-						})
-						.failureHandler(new AuthenticationFailureHandler() {
-							@Override
-							public void onAuthenticationFailure(HttpServletRequest request,
-									HttpServletResponse response, AuthenticationException exception)
-									throws IOException, ServletException {
-								System.out.println("exception : " + exception.getMessage());
-								response.sendRedirect("/error/401");
-							}
-						})
-						.permitAll());
-
-		return http.build();
-
-	}
-
-}
+//package com.demo.config;
+//
+//import java.io.File;
+//import java.security.cert.X509Certificate;
+//
+//import org.opensaml.security.x509.X509Support;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.saml2.core.Saml2X509Credential;
+//import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+//import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
+//import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+//import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+//import org.springframework.security.saml2.provider.service.web.RelyingPartyRegistrationResolver;
+//import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+//import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+////	@Autowired
+////	RelyingPartyRegistrationResolver relyingPartyRegistrationResolver;
+//
+////	@Value("${verification.key}")
+////	File verificationKey;
+////
+////	@Bean
+////	public RelyingPartyRegistrationRepository relyingPartyRegistrations() throws Exception {
+////	    X509Certificate certificate = X509Support.decodeCertificate(this.verificationKey);
+////	    Saml2X509Credential credential = Saml2X509Credential.verification(certificate);
+////	    RelyingPartyRegistration registration = RelyingPartyRegistration
+////	            .withRegistrationId("example")
+////	            .assertingPartyDetails(party -> party
+////	                .entityId("https://idp.example.com/issuer")
+////	                .singleSignOnServiceLocation("https://idp.example.com/SSO.saml2")
+////	                .wantAuthnRequestsSigned(false)
+////	                .verificationX509Credentials(c -> c.add(credential))
+////	            )
+////	            .build();
+////	    return new InMemoryRelyingPartyRegistrationRepository(registration);
+////	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+////		Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver,
+////				new OpenSamlMetadataResolver());
+//
+//		http.csrf(AbstractHttpConfigurer::disable)
+//				.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+//				.saml2Login().and()
+//				.saml2Logout().and();
+////				.addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
+//
+//		return http.build();
+//
+//	}
+//
+//}
